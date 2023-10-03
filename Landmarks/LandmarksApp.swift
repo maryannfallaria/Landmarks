9510b597a73a915b6e8e58ea101910a4b4e4f6f4
maryannfallaria <maufallaria@gmail.com>
2023-10-03 15:15:09 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mary-Ann Fallaria on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
